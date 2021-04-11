baefb166bbdc0d5d7eb1263e13f5a78887103bf3
Rachel Taima <rayuchtai@gmail.com>
2021-04-11 00:16:07 -0700

    added Images

--- js/app.js
+++ js/app.js
@@ -1,41 +1,29 @@
 
+//
 
 
-$(() => {
+$(()=> {
 
 
-  $.ajax({
-     url: "https://rws-cards-api.herokuapp.com/api/v1/cards"
-   }).then(
-     (data) => {
 
 
-       for (let i = 0; i < data.cards.length; i++) {
 
-         let $findFool = data.cards[i]
-         let $foolsValue = $findFool.value_int
 
-         if ($foolsValue === 1) {
-
-              console.log($foolsValue);
-         } else{
-
-              // console.log('Well then....try again?');
-         }
-
-       }
-
-        let $test = data.cards[0].name
+    $.ajax({
+      url:"https://rws-cards-api.herokuapp.com/api/v1/cards"
+    }).then(
+      (data) => {
 
         console.log(data);
-        console.log($test);
+
+      },
+      () => {
+        console.log("bad request");
+      }
+    )
 
 
-     },
-     () => {
-       console.log("bad request");
-     }
-   )
 
 
- })
+
+})
