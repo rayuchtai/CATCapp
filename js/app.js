05346754092deca89844911a449d2e37a1327631
Rachel Taima <rayuchtai@gmail.com>
2021-04-12 15:37:09 -0700

    ...

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-//
+
 
 
 $(()=> { //beginning of the winodws on load function
@@ -10,6 +10,7 @@ $(()=> { //beginning of the winodws on load function
     let randomNum = Math.floor(Math.random()*thing)
     return randomNum
   }
+
 
   let cards = []
 
@@ -29,9 +30,23 @@ $(()=> { //beginning of the winodws on load function
 
           let index = randomNumber(cards.length)
           let currentCard = cards[index]
+          let cardImage = $('.card-image')
+
+
+          const whichCard = () => {
+              let roll = randomNumber(6)
+
+              if (roll % 2 === 0) {
+                cardImage.css({'trandform':'rotate(180deg)'})
+              } else {
+                cardImage
+              }
+            return whichCard
+          }
 
           const $modal = $('#modal')
-          let $modalInnards = $('#modal=innards')
+          let $modalInnards = $('#modal-innards')
+
 
 
 
@@ -40,15 +55,23 @@ $(()=> { //beginning of the winodws on load function
 
           $(".card-back").on('click', () => { //flipping the card
 
+
             $cardContainer
             .html(`<h3 class="card-name">${currentCard.name}</h3>\
             <img src="imgs/card-back.jpg" class="card-back" alt="card-back"/>`)
 
             $('.card-back')
-            .replaceWith(`<img src="imgs/${currentCard.name}.jpg" class="card-image" alt="" />`)
+            .replaceWith(`<input type="image" src="imgs/${currentCard.name}.jpg" class="card-image" id="open-modal" alt=""/>`)
 
-            $(".card-image").on('click', () => {
 
+
+            $("#open-modal").on('click', () => {
+              $('#modal').css('display', 'block')
+              console.log('it is clickable');
+            })
+
+            $('#closeBtn').on('click', () => {
+              $('#modal').css('display', 'none')
             })
 
           }) //do not delete
