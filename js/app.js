76fcf50bb00abfbcec3581d774eae3717a7f950a
Rachel Taima <rayuchtai@gmail.com>
2021-04-11 19:34:07 -0700

    draw button works.

--- js/app.js
+++ js/app.js
@@ -2,19 +2,47 @@
 //
 
 
-$(()=> {
+$(()=> { //beginning of the winodws on load function
 
+  const $cardContainer = $('.card-container')
 
+  const randomNumber = (thing) => {
+    let randomNum = Math.floor(Math.random()*thing)
+    return randomNum
+  }
 
-
+  let cards = []
 
 
     $.ajax({
-      url:"https://rws-cards-api.herokuapp.com/api/v1/cards"
+      url:"https://rws-cards-api.herokuapp.com/api/v1/cards/"
     }).then(
       (data) => {
 
-        console.log(data);
+        for (let i = 0; i < 78; i++) {
+              cards.push(data.cards[i])
+        }
+
+        console.log(cards);
+
+        $('#draw').on('click', () => {
+
+          let index = randomNumber(cards.length)
+          let currentCard = cards[index]
+
+          $($cardContainer)
+            .html(
+              `<h3 class="card-name">${currentCard.name}</h3>\
+                  <input type="image" src="imgs/${currentCard.name}.jpg"class="card-image"alt=""/>`
+              )
+
+          $(".card-image").on('click')
+
+        }) //do not delete
+
+
+
+
 
       },
       () => {
@@ -26,4 +54,4 @@ $(()=> {
 
 
 
-})
+}) // the end of the windows on load function
